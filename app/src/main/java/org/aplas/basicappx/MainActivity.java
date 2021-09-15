83f2808691eb599586374c95c54036ab0660d892
Genadi Dharma <genadislawa@gmail.com>
2021-09-15 11:20:14 +0800

    4 - make fields and methods in mainactivity

--- app/src/main/java/org/aplas/basicappx/MainActivity.java
+++ app/src/main/java/org/aplas/basicappx/MainActivity.java
@@ -3,12 +3,61 @@ package org.aplas.basicappx;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Distance dist = new Distance();
+    private Weight weight = new Weight();
+    private Temperature temp = new Temperature();
+    private Button convertBtn;
+    private EditText inputTxt;
+    private EditText outputTxt;
+    private Spinner unitOri;
+    private Spinner unitConv;
+    private RadioGroup unitType;
+    private CheckBox roundBox;
+    private CheckBox formBox;
+    private ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected double convertUnit(String type, String oriUnit, String convUnit, double value) {
+        double result;
+
+        switch (type) {
+            case "Temperature":
+                result = temp.convert(oriUnit, convUnit, value);
+                break;
+            case "Distance":
+                result = dist.convert(oriUnit, convUnit, value);
+                break;
+            case "Weight":
+                result = weight.convert(oriUnit, convUnit, value);
+                break;
+            default:
+                result = 0;
+        }
+
+        return result;
+    }
+
+    protected String strResult(double val, boolean rounded) {
+        if (rounded) {
+            return new DecimalFormat("#.##").format(val);
+        } else {
+            return new DecimalFormat("#.#####").format(val);
+        }
     }
 }
